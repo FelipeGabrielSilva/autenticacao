554cd91f42c43b95d501b82933b584ed71d5ffb7
Felipe Gabriel <felipegabriel1106@outlook.com>
2024-04-30 13:31:55 -0300

    top

--- prisma/migrations/20240430163143_/migration.sql
+++ prisma/migrations/20240430163143_/migration.sql
@@ -22,20 +22,5 @@ CREATE TABLE `Post` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- CreateTable
-CREATE TABLE `Permission` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userId` INTEGER NOT NULL,
-    `action` ENUM('MANAGE', 'CREATE', 'READ', 'UPDATE', 'DELETE') NOT NULL,
-    `subject` ENUM('USER', 'POST', 'ALL') NOT NULL,
-    `conditions` VARCHAR(191) NOT NULL,
-
-    UNIQUE INDEX `Permission_userId_action_subject_key`(`userId`, `action`, `subject`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 -- AddForeignKey
 ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Permission` ADD CONSTRAINT `Permission_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

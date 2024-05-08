9a7c4488fe5173d592b05e4bc7567338cd9e83d7
Felipe Gabriel <felipegabriel1106@outlook.com>
2024-05-08 09:25:04 -0300

    feat(code): code dix CASL

--- src/casl/casl-ability.factory/casl-ability.factory.ts
+++ src/casl/casl-ability.factory/casl-ability.factory.ts
@@ -1,5 +1,5 @@
-import { AbilityBuilder, PureAbility, createMongoAbility } from "@casl/ability";
-import { PrismaQuery, Subjects } from "@casl/prisma";
+import { AbilityBuilder, PureAbility } from "@casl/ability";
+import { PrismaQuery, Subjects, createPrismaAbility } from "@casl/prisma";
 import { Injectable } from "@nestjs/common";
 import { Post as PrismaPost, User } from "@prisma/client";
 import { Action } from "../authorization/action.type";
@@ -12,7 +12,7 @@ type AppAbility = PureAbility<[string, 'all' | Subjects<{
 @Injectable()
 export class CaslAbilityFactory {
     createForUser(user: User) {
-        const { can, cannot, build } = new AbilityBuilder<AppAbility>(createMongoAbility);
+        const { can, cannot, build } = new AbilityBuilder<AppAbility>(createPrismaAbility);
 
         if (user.isAdmin) {
             can(Action.Manage, 'all');
